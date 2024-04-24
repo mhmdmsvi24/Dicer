0343c8fc2cc5102b55ef394ed23b29579cff390b
MhmdMsvi <mohammadmousavig@gmail.com>
2024-04-24 13:48:07 +0330

    feat: roll functionality added

--- Level-1/script.js
+++ Level-1/script.js
@@ -1,1 +1,32 @@
 // Game Logic (Roll, Hold, Scores)
+
+// Players
+const redPlayer = {
+  name: "red",
+  pos: "left",
+  score: 0
+}
+
+const bluePlayer = {
+  name: "blue",
+  pos: "right",
+  score: 0
+}
+
+// Roll
+const rollBtn = document.getElementById("btn-roll");
+rollBtn.addEventListener("click", () => roll(randNum))
+
+function roll(randNum) {
+ clearDice();
+ const diceToShow = document.getElementById(`dice-${randNum()}`);
+ diceToShow.classList.remove("d-none");
+}
+
+function clearDice() {
+  for (let i = 1; i <= 6; i += 1) {
+    document.getElementById(`dice-${i}`).classList.add("d-none");
+  }
+}
+
+const randNum = () => Math.trunc(Math.random() * 6) + 1;
